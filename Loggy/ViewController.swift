75a3717fd3337b7f40871dabf4de4a73dad683a2
Peter Strand <peter@nena.se>
2017-06-19 19:36:08 -0400

    Add initial project file and app template
    One iOS target with single view app from template
    created with Xcode9-beta.
    No other changes except changing deployment
    target to iOS 10.0.

---
+++ Loggy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Loggy
+//
+//  Created by Peter Strand on 2017-06-19.
+//  Copyright © 2017 Peter Strand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
