f059f5b5357d96e63064c574f3590117460994fa
Peter Strand <peter@nena.se>
2017-06-19 20:18:20 -0400

    Do the usual renamings to avoid conflicts
    Change name of viewcontroller, appdelegate,
    assets, info plist and so on to avoid confusion
    when having projects/workspaces open with multiple
    apps.

--- Loggy/LoggyAppDelegate.swift
+++ Loggy/LoggyAppDelegate.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class LoggyAppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
