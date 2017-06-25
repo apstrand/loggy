74827845dd2aa32c052480b1837c7a7c48adb3b4
Peter Strand <peter@nena.se>
2017-06-24 21:29:21 -0400

    Add some basic gps logging and gpx saving

--- Loggy/DashboardViewController.swift
+++ Loggy/DashboardViewController.swift
@@ -8,9 +8,19 @@
 
 import UIKit
 import MapKit
+import LoggyTools
 
 class DashboardViewController: UIViewController {
 
+  struct SettingNames {
+    static let PowerSave = "power_save"
+    static let AutoWaypoint = "auto_waypoint"
+  }
+
+  enum Failure: Error {
+    case msg(String)
+  }
+  
   @IBOutlet weak var locationLabel: UILabel!
   @IBOutlet weak var altitudeLabel: UILabel!
   @IBOutlet weak var speedLabel: UILabel!
@@ -22,40 +32,167 @@ class DashboardViewController: UIViewController {
   @IBOutlet weak var mapView: MKMapView!
   @IBOutlet weak var autoWaypointToggle: UISwitch!
   @IBOutlet weak var powerSaveToggle: UISwitch!
-  @IBOutlet weak var startButton: UIButton!
+  @IBOutlet weak var trackingToggle: UISwitch!
   @IBOutlet weak var waypointButton: UIButton!
-  @IBOutlet weak var stopButton: UIButton!
+  
+  
+  let gps : GPSTracker
+  
+  class TapHandler {
+    let callback : (UIView) -> Void
+    let view : UIView
+    init(_ view : UIView, _ cb: @escaping (UIView) -> Void) {
+      self.view = view
+      self.callback = cb
+      let tapGesture = UITapGestureRecognizer()
+      tapGesture.numberOfTapsRequired = 1
+      tapGesture.addTarget(self, action: #selector(TapHandler.tapDetected(target:)))
+      view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func tapDetected(target: Any) {
+      callback(view)
+    }
+  }
+
+  var tapHandlers : [TapHandler] = []
+  required init?(coder aCoder: NSCoder) {
+    gps = GPSTracker()
+    super.init(coder: aCoder)
+  }
+  
+  class Logger {
+    static let dateFormatter : DateFormatter = {
+      let fmt = DateFormatter()
+      fmt.dateFormat = "yyyyMMdd_hhmmss"
+      return fmt
+    }()
+    let gpx_path : URL
+    let gpx_filename : URL
+    var tracks : [GPSTracker.TrackPoint] = []
+    var waypoints : [GPSTracker.TrackPoint] = []
+    
+    init() throws {
+      let name = "track-" + Logger.dateFormatter.string(from: Date()) + ".gpx"
+      let dir = URL(fileURLWithPath: NSTemporaryDirectory())
+      //  else { throw Failure.msg("Cannot create temporary path") }
+      let fn = URL(fileURLWithPath: name)
+      //  else { throw Failure.msg("Cannot create gpx file name") }
+      gpx_path = dir.appendingPathComponent(name)
+      gpx_filename = fn
+    }
+    
+    func log_point(_ loc : GPSTracker.TrackPoint) {
+      tracks.append(loc)
+    }
+    
+    func log_waypoint(_ pt : GPSTracker.TrackPoint) {
+      waypoints.append(pt)
+    }
+    
+    func finish() {
+      var str = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+      str += "<gpx xmlns=\"http://www.topografix.com/GPX/1/0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd\" version=\"1.0\" creator=\"se.nnea.loggy\">\n"
+      
+      str += " <trk>\n"
+      str += "  <name>Track</name>\n"
+      str += "  <trkseg>\n"
+      for track in tracks {
+        let lat = track.location.coordinate.latitude
+        let lon = track.location.coordinate.longitude
+        str += " <trkpt lat=\"\(lat)\" lon=\"\(lon)\"></trkpt>\n"
+      }
+      str += "  </trkseg>\n"
+      str += " </trk>\n"
+      for waypoint in waypoints {
+        let lat = waypoint.location.coordinate.latitude
+        let lon = waypoint.location.coordinate.longitude
+        str += " <wpt lat=\"\(lat)\" lon=\"\(lon)\"></wpt>\n"
+      }
+      str += "</gpx>\n"
+      try? str.write(to: gpx_path, atomically: true, encoding: .utf8)
+      
+      let fm = FileManager.default
+      let cloud_dir = fm.url(forUbiquityContainerIdentifier: nil)
+      print(cloud_dir)
+      guard let cloud_path = cloud_dir?.appendingPathComponent("Documents").appendingPathComponent(gpx_filename.lastPathComponent)
+        else {
+        print("Cannot create icloud path")
+        return
+      }
+      
+      print(cloud_path)
+      let fc = NSFileCoordinator()
+      
+      do {
+        try fm.setUbiquitous(true, itemAt: gpx_path, destinationURL: cloud_path)
+      } catch let err {
+        print("Failed to move gpx file to icloud: \(err)")
+        
+      }
+      /*
+      var error : NSError?
+      fc.coordinate(writingItemAt: cloud_path, options: [.forReplacing], error: &error, byAccessor: { url in
+        print(url)
+        do {
+          try str.write(to: url, atomically: true, encoding: .utf8)
+        } catch let err {
+          print("Failed to write gpx file: \(err)")
+        }
+      })
+ */
+    }
+  }
+  
+  var logger : Logger?
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    tapHandlers.append(contentsOf: [
+      TapHandler(locationValue, { self.toggleUnit($0) }),
+      TapHandler(speedValue, { self.toggleUnit($0) }),
+      TapHandler(altitudeValue, { self.toggleUnit($0) }),
+      TapHandler(bearingValue, { self.toggleUnit($0) })
+    ])
+    
+    let defaults = UserDefaults.standard
+    trackingToggle.isOn = false
+    powerSaveToggle.isOn = defaults.bool(forKey: SettingNames.PowerSave)
+    autoWaypointToggle.isOn = defaults.bool(forKey: SettingNames.AutoWaypoint)
+    
+    gps.setTrackLogger({ pt in self.logger?.log_point(pt) } )
+  }
+  
+  func toggleUnit(_ sender: UIView) {
     
   }
   
-  @IBAction func startTracking(sender: UIButton) {
-    
-  }
-  @IBAction func stopTracking(sender: UIButton) {
-    
-  }
   @IBAction func storeWaypoint(sender: UIButton) {
-    
+    gps.storeWaypoint()
+  }
+  
+  @IBAction func toggleTracking(sender: UISwitch) {
+    if sender.isOn {
+      try? logger = Logger()
+      gps.start()
+    } else {
+      gps.stop()
+      logger?.finish()
+    }
   }
   
   @IBAction func toggleAutowaypoint(sender: UISwitch) {
     
-  }
-  @IBAction func togglePowerSave(sender: UISwitch) {
     
   }
-  
-  @IBAction func toggleUnit(sender: UILabel) {
+
+  @IBAction func togglePowerSave(sender: UISwitch) {
     
   }
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
   
 }
--- LoggyTools/GPSTracker.swift
+++ LoggyTools/GPSTracker.swift
@@ -7,30 +7,150 @@
 //
 
 import Foundation
-
+import CoreLocation
 
 public class GPSTracker {
   
+  public struct TrackPoint {
+    public let location : CLLocation
+  }
+  
+  public typealias TrackLogger = (TrackPoint) -> Void
+  
+  var logger : TrackLogger?
+  let loc_mgr : CLLocationManager
+  var pendingTracking = false
+  var loc_delegate : LocDelegate! = nil
+  
   public init() {
+    loc_mgr = CLLocationManager()
+    let delegate = LocDelegate(self)
+    loc_mgr.delegate = delegate
+    loc_delegate = delegate
+  }
+  
+  func startPendingTracking() {
+    if pendingTracking {
+      loc_mgr.startUpdatingLocation()
+    }
     
+    
+  }
+  
+  public func setTrackLogger(_ logger : @escaping TrackLogger) {
+    self.logger = logger
   }
   
   public func start() {
-  
+    pendingTracking = true
+    
+    let status = CLLocationManager.authorizationStatus()
+    print("start: gps status: \(status.rawValue)")
+    
+    switch status {
+    case .notDetermined:
+      // loc_mgr?.requestWhenInUseAuthorization()
+      loc_mgr.requestAlwaysAuthorization()
+      break
+    case .authorizedAlways, .authorizedWhenInUse:
+      startPendingTracking()
+    default:
+      break
+    }
   }
   
   public func stop() {
-    
+    pendingTracking = false
   }
   
   public func storeWaypoint() {
     
   }
   
-  func retrieveData() {
+  class LocDelegate : NSObject, CLLocationManagerDelegate {
+    let parent : GPSTracker
+    init(_ gps : GPSTracker) {
+      parent = gps
+    }
+    func locationManager(_ : CLLocationManager, didUpdateLocations locs: [CLLocation]) {
+//      print("Tells the delegate that new location data is available: [\(locs)]")
+      if let logger = parent.logger {
+        for loc in locs {
+          let tp : TrackPoint = TrackPoint(location: loc)
+          logger(tp)
+        }
+      }
+    }
+    
+    func locationManager(_ : CLLocationManager, didFailWithError: Error) {
+      print("Tells the delegate that the location manager was unable to retrieve a location value.")
+    }
+    
+    func locationManager(_ : CLLocationManager, didFinishDeferredUpdatesWithError: Error?) {
+      print("Tells the delegate that updates will no longer be deferred.")
+    }
+    
+    func locationManagerDidPauseLocationUpdates(_ : CLLocationManager) {
+      print("Tells the delegate that location updates were paused.")
+    }
+    
+    func locationManagerDidResumeLocationUpdates(_ _ : CLLocationManager) {
+      print("Tells the delegate that the delivery of location updates has resumed.\nResponding to Heading Events")
+    }
+    
+    func locationManager(_ : CLLocationManager, didUpdateHeading: CLHeading) {
+      print("Tells the delegate that the location manager received updated heading information.")
+    }
+    
+    func locationManagerShouldDisplayHeadingCalibration(_ : CLLocationManager) -> Bool {
+      print("Asks the delegate whether the heading calibration alert should be displayed.\nResponding to Region Events")
+      return false
+    }
+    
+    func locationManager(_ : CLLocationManager, didEnterRegion: CLRegion) {
+      print("Tells the delegate that the user entered the specified region.")
+    }
+      
+    func locationManager(_ : CLLocationManager, didExitRegion: CLRegion) {
+      print("Tells the delegate that the user left the specified region.")
+    }
+    func locationManager(_ : CLLocationManager, didDetermineState: CLRegionState, for: CLRegion) {
+      print("Tells the delegate about the state of the specified region.")
+    }
+    func locationManager(_ : CLLocationManager, monitoringDidFailFor: CLRegion?, withError: Error) {
+      print("Tells the delegate that a region monitoring error occurred.")
+    }
+    
+    func locationManager(_ : CLLocationManager, didStartMonitoringFor: CLRegion) {
+      print("Tells the delegate that a new region is being monitored.\nResponding to Ranging Events")
+    }
+    
+    func locationManager(_ : CLLocationManager, didRangeBeacons: [CLBeacon], in: CLBeaconRegion) {
+      print("Tells the delegate that one or more beacons are in range.")
+    }
+    
+    func locationManager(_ : CLLocationManager, rangingBeaconsDidFailFor: CLBeaconRegion, withError: Error) {
+      print("Tells the delegate that an error occurred while gathering ranging information for a set of beacons.\nResponding to Visit Events")
+    }
+    
+    func locationManager(_ : CLLocationManager, didVisit: CLVisit) {
+      print("Tells the delegate that a new visit-related event was received.\nResponding to Authorization Changes")
+    }
+  
+    func locationManager(_ : CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+      
+      print("didChangeAuthorization: gps status: \(status.rawValue)")
+      
+      switch status {
+      case .authorizedAlways, .authorizedWhenInUse:
+        parent.startPendingTracking()
+        break
+      default:
+        break
+      }
+    }
     
   }
   
-  
-  
 }
+
